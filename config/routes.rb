ed59fa4bd9a8104460226893c6956c9740083aa5
spiritusstribens <spiritusstribens@gmail.com>
2015-05-30 16:52:25 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+	root 'welcome#index'
+	resources :users
+	resources :yochats do
+		resources :comments
+	end
+	namespace :admin do
+		resources :users
+		resources :yochats
+		resources :comments
+	end
 	# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
